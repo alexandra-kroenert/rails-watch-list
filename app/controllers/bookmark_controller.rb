0061d0db4d7f069e56345e2892ebb5546db295e0
Alexandra Kroenert <djbongles@gmail.com>
2022-04-19 21:32:08 +0200

    part of the controllers and routes added

---
+++ app/controllers/bookmark_controller.rb
@@ -0,0 +1,22 @@
+class BookmarkController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'bookmark/new'
+  get 'bookmark/create'
+  get 'bookmark/destroy'
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    #   resources :reviews, only: :create
+    # end
+    resources :bookmarks, only: :destroy
+    # resources :reviews, only: :destroy
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
